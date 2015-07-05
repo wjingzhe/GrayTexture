6548ab02c526607253607b6fb963caf135ab2bfb
Jingz <455786463@qq.com>
2015-07-06 00:59:41 +0800

    delete unnessery pram in glsl and replace the deprecated interface

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -9,20 +9,16 @@ static const GLchar* s_szColorSpriteVSH =
 	"													\n\
 	attribute vec4 a_position;							\n\
 	attribute vec2 a_texCoord;							\n\
-	attribute vec4 a_color;								\n\
 														\n\
 	#ifdef GL_ES										\n\
-	varying lowp vec4 v_fragmentColor;					\n\
 	varying mediump vec2 v_texCoord;					\n\
 	#else												\n\
-	varying vec4 v_fragmentColor;						\n\
 	varying vec2 v_texCoord;							\n\
 	#endif												\n\
 														\n\
 	void main()											\n\
 	{													\n\
 		gl_Position = CC_MVPMatrix * a_position;		\n\
-		v_fragmentColor = a_color;						\n\
 		v_texCoord = a_texCoord;						\n\
 	}";													
 //--------------------------------------------------------
@@ -34,7 +30,6 @@ static const GLchar* s_szColorSpriteFSH =
 																														\n\
 	uniform sampler2D u_texture; 				 																		\n\
 	varying vec2 v_texCoord; 					 																		\n\
-	varying vec4 v_fragmentColor; 																						\n\
 																														\n\
 	void main(void) 							 																		\n\
 	{ 											 																		\n\
@@ -108,7 +103,7 @@ bool HelloWorld::init()
 
 	 texture2d = TextureCache::sharedTextureCache()->addImage("HelloWorld.png");
     
-	texture2d->getGLProgram()->initWithVertexShaderByteArray( s_szColorSpriteVSH, s_szColorSpriteFSH );  
+	 texture2d->getGLProgram()->initWithByteArrays(s_szColorSpriteVSH, s_szColorSpriteFSH);
 
 	
 
